9f3cf9b134a84878add52d78ad14e1768b8e9d18
youngleek0918 <younglee0918@gmail.com>
2021-01-15 10:44:38 +0900

    get all movies & build movie rows

---
+++ src/Row.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import axios from './axios';
+import "./Row.css"
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs based on a specific condition/varaible
+  useEffect(() => {
+    async function fetchData() {
+      // fetchUrl from App.js
+      // ex) https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213
+      const request = await axios.get(fetchUrl);
+
+      // console.log(request)
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+    // if [], run once when the row loads, and dont run again
+  }, [fetchUrl]);
+
+  // console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {/* several row__poster(s) */}
+        {movies.map(movie => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Row
