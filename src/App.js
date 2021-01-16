fe1ab8d669ee9d2292b7f370f4fa25da26d7e513
youngleek0918 <younglee0918@gmail.com>
2021-01-16 10:36:34 +0900

    Built navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import requests from './requests';
 import Row from "./Row";
 import Banner from "./Banner";
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="app">
-      {/* Nav */}
+      <Nav />
 
       <Banner />
       
